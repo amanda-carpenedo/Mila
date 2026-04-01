ebcd0830b9d12e61eddb0cafc73c3cb822e615e2
Amanda Carpenedo <amanda_carpenedo@hotmail.com>
2026-04-01 12:21:10 +0200

    feat: shop e shop single

--- assets/js/components/product-carousel.js
+++ assets/js/components/product-carousel.js
@@ -20,6 +20,12 @@
     thumbs[current].classList.add("is-active");
 
     if (counter) counter.textContent = (current + 1) + "/" + total;
+
+    // Atualiza src de todos os thumbnails para recortes da nova imagem ativa
+    var newSrc = slides[current].src;
+    thumbs.forEach(function (thumb) {
+      thumb.querySelector("img").src = newSrc;
+    });
   }
 
   if (prev) prev.addEventListener("click", function () { goTo(current - 1); });
